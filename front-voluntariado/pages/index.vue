b0ff487faef035a5444ad267f0758df750b85292
Constanza Palomo <constanza.palomo@usach.cl>
2023-06-19 01:11:11 -0400

    some fixes

--- front-voluntariado/pages/index.vue
+++ front-voluntariado/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <header class="shadow-sm bg-#CA8F28">
+      <title>VoluntaRed</title>
       <nav class="container mx-auto p-4 flex justify-between items-center">
         <div>
           <a>
@@ -12,38 +13,16 @@
           </a>
         </div>
         <div class="flex items-center gap-4">
-          <NuxtLink to="/" class="cta-button">VoluntaRed</NuxtLink>
-          <NuxtLink to="/RegistroEmergencia" class="cta-button">Registro Emergencia</NuxtLink>
-          <NuxtLink to="/contact" class="cta-button">Módulo Coordinadores</NuxtLink>
+          <NuxtLink to="/registroUsuario" class="cta-button">Registro</NuxtLink>
+          <NuxtLink to="/loginUsuario" class="cta-button">Login</NuxtLink>
+          <NuxtLink to="/registroEmergencia" class="cta-button">Registro Emergencia</NuxtLink>
+          <NuxtLink to="/vistaMapa" class="cta-button">Ver Mapa</NuxtLink>
+          <NuxtLink to="/listaEmergencia" class="cta-button">Emergencias</NuxtLink>
         </div>
       </nav>
     </header>
-
-    <div class="container">
-      <title>VoluntaRed</title>
-      <v-row justify="center" alling="center">
-        <h1 class="text-center">Sistema de Gestión de Voluntariado Espontáneo</h1>
-        <hr />
-        <!-- Your form elements and buttons go here -->
-      </v-row>
-    </div>
-
-    <footer class="container mx-auto p-4 flex justify-between border-t-2">
-      <ul class="flex gap-4">
-        <li>
-          <NuxtLink to="/" class="cta-button">Home</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/about" class="cta-button">About</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/contact" class="cta-button">Contact</NuxtLink>
-        </li>
-      </ul>
-    </footer>
   </div>
 </template>
-
 
 <script>
 
@@ -56,8 +35,8 @@ export default {
 
 <style>
  body {
-   background-color: #fffffe;
-   color: #192D36;
+   background-color: #FEE9E4;
+   color: #2E5077;
    font-family: ui-rounded, sans-serif;
    margin: 0;
    padding: 0;
@@ -69,7 +48,7 @@ export default {
 }
 body2 {
   background-color: #CA8F28;
-  color: #192D36;
+  color: #2E5077;
   font-family: ui-rounded, sans-serif;
   margin: 0;
   padding: 0;
@@ -80,8 +59,8 @@ body2 {
   text-align: center;
 }
 header {
-  background-color: #175873;
-  color: #192D36;
+  background-color: #F47784;
+  color: #2E5077;
   padding: 0px;
   text-align: center;
 }
@@ -95,29 +74,29 @@ header img {
 .logo h1 {
   font-family: ui-rounded, sans-serif;
   font-size: 36px;
-  color: #192D36;
+  color: #2E5077;
 }
 h1 {
-  background-color: #fffffe;
-  color: #192D36;
+  background-color: #FEE9E4;
+  color: #2E5077;
   font-size: 36px;
   margin-bottom: 20px;
 }
 .content-table {
-  background-color: #fffffe;
-  color: #192D36;
+  background-color: #FEE9E4;
+  color: #2E5077;
   border-collapse: collapse;
   width: 100%;
   margin-top: 20px;
 }
 .content-table th {
-  background-color: #fffffe;
-  color: #192D36;
+  background-color: #FEE9E4;
+  color: #2E5077;
   padding: 10px;
   text-align: left;
 }
 .content-table td {
-  border: 1px solid #fffffe;
+  border: 1px solid #FEE9E4;
   padding: 10px;
 }
 .hero-image {
@@ -131,18 +110,18 @@ h1 {
 }
 .hero-text h2 {
   font-size: 24px;
-  color: #192D36;
+  color: #2E5077;
   margin-bottom: 20px;
 }
 .hero-text p {
   font-size: 24px;
-  color: #192D36;
+  color: #2E5077;
   margin-bottom: 30px;
 }
 .cta-button {
   display: inline-block;
   padding: 12px 24px;
-  background-color: #0C1446;
+  background-color: #FDB35A;
   color: #fff;
   font-size: 20px;
   text-decoration: none;
